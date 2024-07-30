f3ca11faa8514818c58d4c07965621ae9f66c129
kootaeho <0307rnxogh@daum.net>
2024-07-30 20:31:17 +0900

    comment

--- Models/chat.js
+++ Models/chat.js
@@ -8,5 +8,5 @@ const pool = mysql.createPool({
     user: dbconfig.user,          // 데이터베이스 사용자명
     password: dbconfig.password,  // 데이터베이스 비밀번호
     database: dbconfig.database,  // 사용할 데이터베이스
-    debug: false      
+    debug: false                  // 디버그 모드 설정
 })
