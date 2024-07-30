d68d224ce3c511f4be474d1169a40aa14e890a9d
kootaeho <0307rnxogh@daum.net>
2024-07-30 19:02:12 +0900

    Dbconnection

--- Models/chat.js
+++ Models/chat.js
@@ -1,1 +1,12 @@
 const mysql = require('mysql')
+
+const dbconfig = require('./Models/chatInfo_config.json');
+
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host: dbconfig.host,          // 데이터베이스 호스트
+    user: dbconfig.user,          // 데이터베이스 사용자명
+    password: dbconfig.password,  // 데이터베이스 비밀번호
+    database: dbconfig.database,  // 사용할 데이터베이스
+    debug: false      
+})
--- Models/user.js
+++ Models/user.js
@@ -1,1 +1,12 @@
 const mysql = require('mysql')
+
+const dbconfig = require('./Models/userInfo_config.json');
+
+const pool = mysql.createPool({
+    connectionLimit: 10,          // 최대 10개의 연결을 유지
+    host: dbconfig.host,          // 데이터베이스 호스트
+    user: dbconfig.user,          // 데이터베이스 사용자명
+    password: dbconfig.password,  // 데이터베이스 비밀번호
+    database: dbconfig.database,  // 사용할 데이터베이스
+    debug: false                  // 디버그 모드 설정
+});
