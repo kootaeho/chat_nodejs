f361b89af2b79f45f181f47290241efdd9020a81
kootaeho <0307rnxogh@daum.net>
2024-07-31 20:27:56 +0900

    websocket

---
+++ index.js
@@ -0,0 +1,16 @@
+const {createServer} = require("http")
+const app = require("/app")
+const {Server} = require("socket.io")
+
+const httpServer = createServer(app)
+const io = new Server(httpServer,{
+    cors:{
+        origin:"http://localhost:3000"
+    }
+})
+
+require("./utils/io")(io)
+
+httpServer.listen(5001,()=>{
+    console.log("http Server listening on port 5001")
+})
